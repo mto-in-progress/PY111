ad2a29683bd1c913a1dbfab9dd7851def4a17b3a
sam <samiraspb11@gmail.com>
2019-12-28 16:29:28 +0300

    complite task-queue

--- Tasks/a1_my_queue.py
+++ Tasks/a1_my_queue.py
@@ -1,44 +1,57 @@
 """
 My little Queue
 """
-from typing import Any
+from typing import Any, List
+
+_queue: List = []
 
 
 def enqueue(elem: Any) -> None:
-	"""
-	Operation that add element to the end of the queue
+    """
+    Operation that add element to the end of the queue
 
-	:param elem: element to be added
-	:return: Nothing
-	"""
-	print(elem)
-	return None
+    :param elem: element to be added
+    :return: Nothing
+    """
+    global _queue
+    _queue.append(elem)
+    print(elem)
+    return None
 
 
 def dequeue() -> Any:
-	"""
-	Return element from the beginning of the queue. Should return None if no elements.
+    """
+    Return element from the beginning of the queue. Should return None if no elements.
 
-	:return: dequeued element
-	"""
-	return None
+    :return: dequeued element
+    """
+    global _queue
+    if _queue is not None:
+        return _queue.pop(0)
+    else:
+        return None
 
 
 def peek(ind: int = 0) -> Any:
-	"""
-	Allow you to see at the element in the queue without dequeuing it
+    """
+    Allow you to see at the element in the queue without dequeuing it
 
-	:param ind: index of element (count from the beginning)
-	:return: peeked element
-	"""
-	print(ind)
-	return None
+    :param ind: index of element (count from the beginning)
+    :return: peeked element
+    """
+    if ind in range(len(_queue)):
+        return _queue[ind]
+
+    if _queue:
+        return None
+
+    print(ind)
 
 
 def clear() -> None:
-	"""
-	Clear my queue
+    """
+    Clear my queue
 
-	:return: None
-	"""
-	return None
+    :return: None
+    """
+    return _queue.clear()
