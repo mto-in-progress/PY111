f8f39cd27174ab229787d6f0d1f7f43962f33b46
sam <samiraspb11@gmail.com>
2020-01-15 09:38:39 +0300

    задача вроде решена)

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -3,42 +3,53 @@ Priority Queue
 
 Queue priorities are from 0 to 5
 """
-from typing import Any
+from typing import Any, Dict
+
+_enqueue: Dict = {}
 
 
 def enqueue(elem: Any, priority: int = 0) -> None:
-	"""
-	Operation that add element to the end of the queue
+    """
+    Operation that add element to the end of the queue
 
-	:param elem: element to be added
-	:return: Nothing
-	"""
-	return None
+    :param elem: element to be added
+    :return: Nothing
+    """
+    try:
+        _enqueue[priority].append(elem)
+
+    except KeyError:
+        _enqueue[priority] = [elem]
 
 
 def dequeue() -> Any:
-	"""
-	Return element from the beginning of the queue. Should return None if not elements.
+    """
+    Return element from the beginning of the queue. Should return None if not elements.
 
-	:return: dequeued element
-	"""
-	return None
+    :return: dequeued element
+    """
+    if _enqueue:
+        return None
+    else:
+        return _enqueue.pop(0)
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
-	"""
-	Allow you to see at the element in the queue without dequeuing it
+    """
+    Allow you to see at the element in the queue without dequeuing it
 
-	:param ind: index of element (count from the beginning)
-	:return: peeked element
-	"""
-	return None
+    :param ind: index of element (count from the beginning)
+    :return: peeked element
+    """
+    return _enqueue[priority][ind]
 
 
 def clear() -> None:
-	"""
-	Clear my queue
+    """
+    Clear my queue
 
-	:return: None
-	"""
-	return None
+    :return: None
+    """
+    # global _enqueue
+    # _enqueue = {}
+    _enqueue.clear()
--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -1,20 +1,38 @@
 def fib_recursive(n: int) -> int:
-	"""
-	Calculate n-th number of Fibonacci sequence using recursive algorithm
+    """
+    Calculate n-th number of Fibonacci sequence using recursive algorithm
 
-	:param n: number of item
-	:return: Fibonacci number
-	"""
-	print(n)
-	return 0
+    :param n: number of item
+    :return: Fibonacci number
+    """
+    if n < 0:
+        raise ValueError
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
 def fib_iterative(n: int) -> int:
-	"""
-	Calculate n-th number of Fibonacci sequence using iterative algorithm
+    """
+    Calculate n-th number of Fibonacci sequence using iterative algorithm
 
-	:param n: number of item
-	:return: Fibonacci number
-	"""
-	print(n)
-	return 0
+    :param n: number of item
+    :return: Fibonacci number
+    """
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if 1 < n <= 2:
+        return 1
+    else:
+        number = [0, 1]
+        try:
+            for i in range(2, (n + 1)):
+                number.append(number[i - 1] + number[i - 2])
+            return number[n]
+        except IndexError:
+            pass
