f78d2f2b167a1e2d68bcef531b5a17772cda8622
sam <samiraspb11@gmail.com>
2020-01-17 11:29:03 +0300

    реализован бинарный поиск

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,18 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
-	print(elem, arr)
+	high = len(arr) - 1
+	low = 0
+	while low <= high:
+		mid = (high + low) // 2
+		guess = arr[mid]
+		if elem == guess:
+			return mid
+		elif elem < guess:
+			high = mid - 1
+		elif elem > guess:
+			low = mid + 1
 	return None
+
+
+
