a975c7211c6d38b7d1163bd334fc3aedea5eebaa
sam <samiraspb11@gmail.com>
2020-01-20 21:15:12 +0300

    реализован ряд Тейлора для синуса и экспоненты

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,25 +2,51 @@
 Taylor series
 """
 from typing import Union
+import math
+import itertools
 
 
 def ex(x: Union[int, float]) -> float:
-	"""
-	Calculate value of e^x with Taylor series
+    """
+    Calculate value of e^x with Taylor series
 
-	:param x: x value
-	:return: e^x value
-	"""
-	print(x)
-	return 0
+    :param x: x value
+    :return: e^x value
+    """
+
+    ex0 = 1
+    dx = x  # приращение
+    i = 2  # номер приращения
+    while abs(dx) > 0:
+        ex0 = ex0 + dx
+        dx = dx * x / i
+        i = i + 1
+    return ex0
+
+
+print(ex(2))
+print(math.exp(2))
 
 
 def sinx(x: Union[int, float]) -> float:
-	"""
-	Calculate sin(x) with Taylor series
+    """
+    Calculate sin(x) with Taylor series
 
-	:param x: x value
-	:return: sin(x) value
-	"""
-	print(x)
-	return 0
+    :param x: x value
+    :return: sin(x) value
+    """
+    result = x
+    if x == 0:
+        return 0
+    else:
+        for i in itertools.count(1, 1):
+            n = 2 * i + 1
+            result += ((-1) ** i) * (x ** n / math.factorial(n))
+            if 0.001 > (x ** i / math.factorial(i)):
+                break
+        print(x)
+        return result
+
+
+print(sinx(3))
+print(math.sin(3))
